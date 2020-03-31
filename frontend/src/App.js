2a7c6ec275ac1afc2c910cd67af29e97dda6073c
Andrelino Silva <andrelinocom@gmail.com>
2020-03-30 23:57:50 -0300

    Finalizado aplicação WEB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
-  return ( <h1>Hello World</h1>  );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
